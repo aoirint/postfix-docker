be43fb6562d241e01282c9667f85e91a1292c4c3
aoirint <aoirint@gmail.com>
2022-12-09 04:07:23 +0900

    copy vmailbox, virtual

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,12 @@ if [ -f /postfix/master.cf ]; then
   cp /postfix/master.cf /etc/postfix/master.cf
 fi
 
+if [ -f /postfix/vmailbox ]; then
+  cp /postfix/vmailbox /etc/postfix/vmailbox
+fi
+
+if [ -f /postfix/virtual ]; then
+  cp /postfix/virtual /etc/postfix/virtual
+fi
+
 exec "$@"
